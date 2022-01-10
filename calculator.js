6b17e1379eb79c2a5491ffd871eb319c5945ec9c
nashitshayan <shayan.nashit@gmail.com>
2022-01-10 12:55:19 +0530

    fix: calc funcs

--- calculator.js
+++ calculator.js
@@ -1,9 +1,22 @@
 
 // calc functions - multiply and divide by 10^11 is done to make sure JS doesn't give weird results, hopefully.
-const add= (num1, num2)=> ((num1*100000000000)+(num2*100000000000))/100000000000;
-const subtract= (num1, num2)=> ((num1*100000000000)-(num2*100000000000))/100000000000;
-const multiply= (num1, num2)=>((num1*100000000000)*(num2*100000000000))/100000000000;
-const divide= (num1, num2)=> ((num1*100000000000)/(num2*100000000000))/100000000000;
+const add= (num1, num2)=> ((num1*Math.pow(10,11))+(num2*Math.pow(10,11)))/Math.pow(10,11);
+const subtract= (num1, num2)=> ((num1*Math.pow(10,11))-(num2*Math.pow(10,11)))/Math.pow(10,11);
+const multiply= (num1, num2)=> ((num1*Math.pow(10,11))*(num2*Math.pow(10,11)))/Math.pow(10,22);
+const divide= (num1, num2)=> (num1/num2);
 
-console.log(subtract(0.2,0.1));
 
+
+
+const operate = (num1, num2, operator)=>{
+    let operation= {
+        '+': add(num1,num2),
+        '-': subtract(num1,num2),
+        '*': multiply(num1,num2),
+        '/': divide(num1,num2),
+    }
+    return operation[operator];
+}
+
+
+console.log(operate(2,3,'*'));
